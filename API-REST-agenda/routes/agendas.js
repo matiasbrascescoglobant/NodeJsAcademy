ef4511fa42b733aab18dc67e2c4c2d846e373108
matiasbrascescoglobant <matias.brascesco@globant.com>
2021-12-13 19:59:04 -0300

    Modifications to make it a REST API.

--- API-REST-agenda/routes/agendas.js
+++ API-REST-agenda/routes/agendas.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const agendasController = require('../controllers/agendas-controller');
 
-router.get('/read-agendas',agendasController.read_agendas);
-router.post('/add-agendas',agendasController.add_agendas);
-router.delete('/del-agendas/:id',agendasController.delete_agendas);
-router.put('/upd-agendas/:id',agendasController.update_agendas);
+router.get('/agendas',agendasController.read_agendas);
+router.post('/agendas',agendasController.add_agendas);
+router.delete('/agendas/:id',agendasController.delete_agendas);
+router.put('/agendas/:id',agendasController.update_agendas);
 
 module.exports = router;
